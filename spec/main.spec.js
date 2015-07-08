67eefe5adab0bd02c8b449a1ae18ab59b20d7d48
Blake Johnson <blake41@gmail.com>
2015-07-08 17:29:47 -0400

    first iteration

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,16 +1,5 @@
 'use strict';
 
 describe("test rendering, adding ids and handling clicks", function() {
-  var gameView;
-  beforeEach(function() {
-    // setFixtures('<div id="container"></div>'); 
 
-  });
-
-  describe( "#render & addIds", function() {
-    it("should create insert a in HTML when its initialized", function() {
-      run();
-      expect($("body")).toContainHtml('<h1>blake</h1>');
-    });     
-  });
 }
