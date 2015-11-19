40f743bc0d4dd262194c846dc44e20de04886a3d
Alexander Burton <alexanderjburton@gmail.com>
2015-11-19 10:39:31 -0500

    bootstrapped and ready to DOM

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ app = {};
 
 function bootstrap() {
   // instantiate your view object here
-
+  var view = new MainView();
+  app.mainView = view
   // attach your view object to the app object as a property
   // where the name is mainView
-
+  $("body").append(view.el);
   // and append your view object's HTML to the body tag
 }
