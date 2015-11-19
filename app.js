e80a45e8521ffae5d7b91e7e431955d4129e6064
dylan.okeefe <dylanokeefe@gmail.com>
2015-11-19 10:20:33 -0500

    finished

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-app = {};
+var app = app || {};
 
 function bootstrap() {
-  // instantiate your view object here
-
-  // attach your view object to the app object as a property
-  // where the name is mainView
-
-  // and append your view object's HTML to the body tag
+  var mainView = new MainView();
+  app.mainView = mainView;
+  $("body").append(mainView.el);
 }
