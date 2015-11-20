b04c8efeeb87976ae3b8d24f0d59303108940a7d
Lois <tatislois@icloud.com>
2015-11-20 14:07:48 -0500

    strange

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ app = {};
 
 function bootstrap() {
   // instantiate your view object here
+  var mainView = new MainView()
+  app.mainView = mainView;
+  $('body').append(mainView.el)
 
   // attach your view object to the app object as a property
   // where the name is mainView
