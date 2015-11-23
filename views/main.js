ba67edf078d3d424e16dc41501a7321537726f59
gil93 <gvelezjr02@gmail.com>
2015-11-23 18:00:34 -0500

    done

--- views/main.js
+++ views/main.js
@@ -12,4 +12,12 @@
 //    - this function should append the following HTML to $el
 //      <h1>blake</h1>
 //    - it should return the instance of the class
-
+MainView = Backbone.View.extend({
+	initialize : function() {
+		this.render();
+	},
+	render : function() {
+		this.$el.append("<h1>blake</h1>")
+		return this;
+	}
+});
