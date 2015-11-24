fad9c9add996c66076f931651cf7267341f40f83
Andrew Persad <akpersad@gmail.com>
2015-11-24 10:00:25 -0500

    done

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ app = {};
 
 function bootstrap() {
   // instantiate your view object here
-
+  app.mainView = new MyView();
   // attach your view object to the app object as a property
   // where the name is mainView
 
   // and append your view object's HTML to the body tag
+  $('body').append(app.mainView.el);
 }
