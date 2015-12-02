632a9a39ae9edf26fe04290c23d87172ee285813
Giancarlo Martinez <g.r.rmartinez49@gmail.com>
2015-12-02 16:06:18 -0500

    create view and initialize

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 app = {};
 
 function bootstrap() {
-  // instantiate your view object here
-
-  // attach your view object to the app object as a property
-  // where the name is mainView
-
-  // and append your view object's HTML to the body tag
+	app.mainView = new MainView;
+	$('body').append(app.mainView.el);
 }
+
+
